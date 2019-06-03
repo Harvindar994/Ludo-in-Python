c3db7854aaa250b8dc41ffddbd9da7c02ec411ff
Harvindar Singh <41161615+Harvindar994@users.noreply.github.com>
2019-06-03 21:30:00 +0530

    Define a function to print object on Screen

--- Ludo.py
+++ Ludo.py
@@ -19,6 +19,10 @@ class Ballast:
         if color_type == 'green':
             self.Bet_block = [815, 491, 910, 588]
             self.Movers_ballast = pygame.image.load('Media/Image/Movers_ballast/Green.png')
+            self.Home_pos_1 = [596, 482, 652, 536]
+            self.Home_pos_2 = [685, 480, 743, 537]
+            self.Home_pos_3 = [597, 574, 652, 630]
+            self.Home_pos_4 = [686, 573, 743, 631]
             self.Path = ([714, 369, 759, 412], [667, 369, 712, 412], [620, 369, 666, 412], [574, 369, 619, 412],
                          [528, 369, 573, 412],
                          [483, 413, 526, 458], [483, 459, 526, 504], [483, 505, 526, 551], [483, 553, 526, 598],
@@ -44,6 +48,10 @@ class Ballast:
         elif color_type == 'yellow':
             self.Bet_block = [10, 492, 106, 588]
             self.Movers_ballast = pygame.image.load('Media/Image/Movers_ballast/Yellow.png')
+            self.Home_pos_1 = [181, 479, 239, 535]
+            self.Home_pos_2 = [268, 479, 326, 535]
+            self.Home_pos_3 = [178, 572, 236, 629]
+            self.Home_pos_4 = [268, 572, 326, 629]
             self.Path = ([393, 599, 437, 645], [393, 553, 437, 598], [393, 505, 437, 551], [393, 459, 437, 504],
                          [393, 413, 437, 458],
                          [348, 368, 393, 413], [302, 368, 347, 413], [255, 368, 300, 413], [208, 368, 254, 413],
@@ -69,6 +77,10 @@ class Ballast:
         elif color_type == 'red':
             self.Bet_block = [9, 61, 105, 158]
             self.Movers_ballast = pygame.image.load('Media/Image/Movers_ballast/Red.png')
+            self.Home_pos_1 = [181, 68, 239, 123]
+            self.Home_pos_2 = [271, 67, 328, 123]
+            self.Home_pos_3 = [182, 160, 238, 216]
+            self.Home_pos_4 = [271, 161, 328, 216]
             self.Path = ([161, 279, 207, 322], [208, 279, 254, 322], [255, 279, 300, 322], [302, 279, 347, 322],
                          [348, 279, 393, 322],
                          [394, 233, 437, 279], [394, 187, 437, 232], [394, 140, 437, 186], [394, 94, 437, 139],
@@ -94,7 +106,10 @@ class Ballast:
         elif color_type == 'blue':
             self.Bet_block = [814, 62, 912, 158]
             self.Movers_ballast = pygame.image.load('Media/Image/Movers_ballast/Blue.png')
-            self.Home_pos = 0
+            self.Home_pos_1 = [594, 66, 652, 123]
+            self.Home_pos_2 = [685, 68, 740, 122]
+            self.Home_pos_3 = [594, 161, 650, 217]
+            self.Home_pos_4 = [690, 162, 744, 217]
             self.Path = ([484, 46, 527, 92], [484, 94, 527, 139], [484, 140, 527, 186], [484, 187, 527, 232],
                          [484, 233, 527, 279],
                          [528, 279, 573, 324], [574, 279, 619, 324], [620, 279, 666, 324], [667, 279, 712, 324],
@@ -117,15 +132,53 @@ class Ballast:
                          [439, 0, 482, 45], [439, 46, 482, 92], [439, 94, 482, 139], [439, 140, 482, 186],
                          [439, 187, 482, 232], [439, 233, 482, 279])
 
-        self.Movers_pos_1 = 'Home'
-        self.Movers_pos_2 = 'Home'
-        self.Movers_pos_3 = 'Home'
-        self.Movers_pos_4 = 'Home'
+        self.Movers_pos_1 = 'home'
+        self.Movers_pos_2 = 'home'
+        self.Movers_pos_3 = 'home'
+        self.Movers_pos_4 = 'home'
         self.color_type = color_type
         self.Bet_status = True
         self.Bet_flag = False
         self.Bet_itration = None
         self.last_bet_number = None
+
+    @staticmethod
+    def get_med_position(rect):
+        x, y, x1, y1 = rect
+        width = x1 - x
+        height = y1 - y
+        return [int(x+(width/2)), int(y+(height/2))]
+
+    def print_movers_ballast(self):
+        global Game_Window
+        home = 'home'
+        if type(self.Movers_pos_1) == str and self.Movers_pos_1 == home:
+            x, y, x1, y1 = self.Home_pos_1
+            Game_Window.blit(self.Movers_ballast, [x, y])
+        elif type(self.Movers_pos_1) == int:
+            x, y, x1, y1 = self.Path[self.Movers_pos_1 - 1]
+            Game_Window.blit(self.Movers_ballast, [x, y])
+
+        if type(self.Movers_pos_2) == str and self.Movers_pos_2 == home:
+            x, y, x1, y1 = self.Home_pos_2
+            Game_Window.blit(self.Movers_ballast, [x, y])
+        elif type(self.Movers_pos_2) == int:
+            x, y, x1, y1 = self.Path[self.Movers_pos_2 - 1]
+            Game_Window.blit(self.Movers_ballast, [x, y])
+
+        if type(self.Movers_pos_3) == str and self.Movers_pos_3 == home:
+            x, y, x1, y1 = self.Home_pos_3
+            Game_Window.blit(self.Movers_ballast, [x, y])
+        elif type(self.Movers_pos_3) == int:
+            x, y, x1, y1 = self.Path[self.Movers_pos_3 - 1]
+            Game_Window.blit(self.Movers_ballast, [x, y])
+
+        if type(self.Movers_pos_4) == str and self.Movers_pos_4 == home:
+            x, y, x1, y1 = self.Home_pos_4
+            Game_Window.blit(self.Movers_ballast, [x, y])
+        elif type(self.Movers_pos_4) == int:
+            x, y, x1, y1 = self.Path[self.Movers_pos_4 - 1]
+            Game_Window.blit(self.Movers_ballast, [x, y])
 
     @staticmethod
     def collide(mouse_x, mouse_y, rect):
@@ -236,4 +289,5 @@ while True:
 
     Game_Window.blit(Game_background, [0, 0])
     b1.bet_manage(event)
+    b1.print_movers_ballast()
     pygame.display.update()
